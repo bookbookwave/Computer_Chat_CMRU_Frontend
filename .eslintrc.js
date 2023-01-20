601d9db38f57a8003acb2878226fe794fb9b0a80
Wave Watcharin <watcharin@igeargeek.com>
2023-01-20 22:44:22 +0700

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+export default {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    quotes: ['error', 'single'],
+    'no-console': 'off',
+  }
+}
